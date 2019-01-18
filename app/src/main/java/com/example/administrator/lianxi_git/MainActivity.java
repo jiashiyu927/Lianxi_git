9789e1963c42a290c3feb692356c60de07f4d858
user1 <860141239@qq.com>
2019-01-18 15:02:53 +0800

    你好

---
+++ app/src/main/java/com/example/administrator/lianxi_git/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.lianxi_git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public  void showData(){
+
+    }
+
+    public void setData(){}
+
+
+
+}
